f062885c23a0f13bc4e7e2ec6a82e564f567ddf2
Keiner Hurtado <keinerhurtado2805@gmail.com>
2026-06-08 13:10:32 +0200

    argumento y malloc del tamaño sin espacios

--- push_swap.c
+++ push_swap.c
@@ -3,26 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: khurtado <khurtado@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 09:07:59 by omarquez          #+#    #+#             */
-/*   Updated: 2026/06/05 09:25:13 by omarquez         ###   ########.fr       */
+/*   Updated: 2026/06/08 13:07:42 by khurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+size_t	ft_strlen_without_spaces(const char *s)
+{
+	size_t	counter;
+	size_t	spaces;
+	size_t	resta;
+	
+	counter = 0;
+	spaces = 0;
+	while (s[counter])
+	{
+		if(s[counter] == ' ')
+			spaces++;
+		counter++;
+	}
+	resta = counter - spaces;
+	//printf ("contiene %ld numeros", resta);
+	return (counter - spaces);
+}
+
+int	ft_to_array(char *str)
+{
+	int *array;
+	
+
+	array = malloc(4 * ft_strlen_without_spaces(str));
+	ft_assing_to_array(array);
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	/*
-	we can use libft, ftprintf and even gnl
-	we are going to recieve the stack a from argv
-	we are receiving some kind of flag to select the algorithm 
-	like: --simple, --medium, etc...
-	*/
+	
 
-	write(1, "this is push_swap",17);
+	if (argc > 1)
+		printf("%s",argv[1]);
+	ft_to_array(argv[1]);
+	//write(1, "this is push_swap",17);
 	return (0);
 }
