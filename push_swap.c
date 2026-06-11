e21b3397bd9a9bc385d800e2ec6b616b2eeb3bee
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-06-11 10:43:42 +0200

    remerge xdd

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 09:07:59 by omarquez          #+#    #+#             */
-/*   Updated: 2026/06/11 10:34:56 by omarquez         ###   ########.fr       */
+/*   Updated: 2026/06/11 10:43:10 by omarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	main(int argc, char **argv)
 		while (result[i])
 		{
 			ft_printf("word[%d] = %s\n", i, result[i]);
-			ft_printf("is valid?", ft_is_valid_input(result[i]));
+			ft_printf("is valid gg?", ft_is_valid_input(result[i]));
 			i++;
 		}
 	}
