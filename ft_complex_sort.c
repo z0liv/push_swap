80b6581ce13d07c021e86210914a241abfaeb343
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-07-02 13:54:52 +0200

    added ft_complex_sort

--- ft_complex_sort.c
+++ ft_complex_sort.c
@@ -6,15 +6,48 @@
 /*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 10:33:57 by omarquez          #+#    #+#             */
-/*   Updated: 2026/07/01 09:28:36 by omarquez         ###   ########.fr       */
+/*   Updated: 2026/07/02 13:54:08 by omarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	ft_max_bits(int num)
+{
+	int	bits;
+
+	bits = 0;
+	while (num != 0)
+	{
+		num = num / 2;
+		bits ++;
+	}
+	return (bits);
+}
+
 void	ft_complex_sort(t_stack *stack_a, t_bench *bench)
 {
-	(void)stack_a;
-	(void)bench;
-	ft_printf("FT-COMPLEX-SORT \n");
+	t_stack	*stack_b;
+	int		counter1;
+	int		counter2;
+	int		max_bits;
+
+	counter1 = 0;
+	stack_b = ft_newstack();
+	max_bits = ft_max_bits(stack_a->size - 1);
+	while (counter1 < max_bits)
+	{
+		counter2 = stack_a->size;
+		while (counter2-- > 0)
+		{
+			if (((stack_a->head->norm_index >> counter1) & 1) == 1)
+				ft_rotate_dlst(stack_a, 'a', bench);
+			else
+				ft_push_dlst(stack_a, stack_b, bench, "b");
+		}
+		while (stack_b->size)
+			ft_push_dlst(stack_b, stack_a, bench, "a");
+		counter1 ++;
+	}
+	ft_dlstclear(stack_b, stack_b->size);
 }
